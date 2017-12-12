1bc55e9d81aa85e894d6d056f9e37a57848eff9a
Akihisa <sumiakihisa@sumiakihisanoMacBook-Pro.local>
2017-12-12 22:28:25 +0900

    [Added] serchHorse.py

---
+++ serchHorse.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+from selenium import webdriver
+
+def serch():
+    search_word = "ロックディスタウン"
+    browser = webdriver.Chrome('./chromedriver')
+    browser.get('http://db.netkeiba.com/?pid=horse_top')
+    time.sleep(1)
+    search_input = browser.find_element_by_css_selector(".field")
+    search_input.send_keys(search_word)
+    search_input.submit()
+    time.sleep(1)
+    print(browser.current_url)
+    return browser.current_url
