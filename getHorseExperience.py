0694149d8b82f4210e5712b8ce69c664c3f8871e
taichi <azumade@higa.local>
2017-12-13 23:36:32 +0900

    add except for non race info horse

--- getHorseExperience.py
+++ getHorseExperience.py
@@ -11,24 +11,33 @@ def main():
     serchedUrl = serchHorse.serch()
     html = urllib2.urlopen(serchedUrl)
     soup = BeautifulSoup(html, 'html.parser')
-    table = soup.findAll('table',{'class':'db_h_race_results'})[0]
-    rows = table.findAll('tr')
-    csvFile = open('raceInfo.csv', 'w')
-    writer = csv.writer(csvFile)
-
+    
     try:
-        for row in rows:
-            csvRow = []
-            for cell in row.findAll(['td', 'th']):
-                if '\n' in cell.get_text():
-                    tmpText = cell.get_text()
-                    updatedText = tmpText.replace('\n', '')
-                    csvRow.append(updatedText)
-                else:
-                    csvRow.append(cell.get_text())
-            writer.writerow(csvRow)
-    finally:
-        csvFile.close()
+        table = soup.findAll('table',{'class':'db_h_race_results'})[0]
+        rows = table.findAll('tr')
+        csvFile = open('raceInfo.csv', 'w')
+        writer = csv.writer(csvFile)
+        try:
+            for row in rows:
+                csvRow = []
+                for cell in row.findAll(['td', 'th']):
+                    if '\n' in cell.get_text():
+                        tmpText = cell.get_text()
+                        updatedText = tmpText.replace('\n', '')
+                        csvRow.append(updatedText)
+                    else:
+                        csvRow.append(cell.get_text())
+                writer.writerow(csvRow)
+        finally:
+            csvFile.close()
+                
+    except:
+        print 'non race experience'
+        urlArray = [serchedUrl]
+        nonTableFile = open('nonRaceInfo.csv', 'w')
+        nonTableWriter = csv.writer(nonTableFile)
+        nonTableWriter.writerow(urlArray)
+        nonTableFile.close()
 
 if __name__ == '__main__':
     main()
