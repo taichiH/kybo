eb5e663586a1a517218ec5b50b22194147ba5b33
akihisa-sumi <sumi.akihisa@gmail.com>
2018-02-25 23:28:16 +0900

    [Fixed] RandamForest.py

--- RandamForest.py
+++ RandamForest.py
@@ -3,30 +3,50 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing
 
+labels = ['name', 'age', 'evaluation', 'venue', 'race_name', 'heads', 'horsenumber', \
+              'popularity', 'horseman', 'distance', 'course_status', 'horse_weight', \
+              'horse_weight_ratio', 'winning_percentage']
+
 train_df = pd.read_csv('TRAIN.csv', low_memory = False)
 train_order_data = train_df['order']
 train_feature_data  = train_df.drop('order', axis = 1)
 label_encoder = preprocessing.LabelEncoder()
-for label in ['name', 'age', 'evaluation', 'venue', 'race_name', 'heads', 'horsenumber', \
-              'popularity', 'horseman', 'distance', 'course_status', 'horse_weight', \
-              'horse_weight_ratio', 'winning_percentage']:
-  train_feature_data[label] = label_encoder.fit_transform(train_feature_data[label])
+for label in labels:
+	train_feature_data[label] = label_encoder.fit_transform(train_feature_data[label])
+
 
 test_feature_data = pd.read_csv('TEST.csv', low_memory = False)
+names = list(test_feature_data['name'].values.flatten())
 label_encoder = preprocessing.LabelEncoder()
-for label in ['name', 'age', 'evaluation', 'venue', 'race_name', 'heads', 'horsenumber', \
-              'popularity', 'horseman', 'distance', 'course_status', 'horse_weight', \
-              'horse_weight_ratio', 'winning_percentage']:
-  test_feature_data[label] = label_encoder.fit_transform(test_feature_data[label])
+for label in labels:
+	test_feature_data[label] = label_encoder.fit_transform(test_feature_data[label])
 
 print 'Training Data: ' + str(train_feature_data.shape[0])
-print 'Test Data: ' + str(test_feature_data.shape[0])
+print 'Test Data: ' + str(test_feature_data.shape[0]) + '\n'
 
 random_forest = RandomForestRegressor(n_estimators = 210, max_depth = 50, n_jobs = 2)
 random_forest.fit(train_feature_data, train_order_data)
 
-print 'feature_importances_:\n' + str(random_forest.feature_importances_)
+feature_importances = random_forest.feature_importances_
+if len(labels) == len(feature_importances):
+	for index in range(len(labels)):
+		print str(labels[index]) + ' : ' + str(round(feature_importances[index] * 100,2)) + '[%]'
 
-predict = random_forest.predict(test_feature_data)
-print 'predict:'
-print predict
+print '\n'
+
+COUNTER = 10
+predicts_list = []
+average_predicts_list = []
+for index in range(COUNTER):
+	predicts = random_forest.predict(test_feature_data)
+	predicts_list.append(predicts)
+
+for names_index in range(len(names)):
+	average_predict = 0
+	for index in range(len(predicts_list)):
+		average_predict = average_predict + predicts_list[index][names_index]
+	average_predicts_list.append(average_predict / COUNTER)
+
+if len(names) == len(average_predicts_list):
+	for index in range(len(names)):
+		print str(names[index]) + ' : ' + str(round(predicts[index] * 100,2)) + '[%]'
