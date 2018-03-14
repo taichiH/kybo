e634282f71d714c1803d481ed9f293b248733585
akihisa-sumi <sumi.akihisa@gmail.com>
2018-03-15 00:11:26 +0900

    [Updated] RandamForest.py

--- horse-rasing/RandamForest.py
+++ horse-rasing/RandamForest.py
@@ -1,45 +1,49 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing
 from collections import Counter
 
-labels = ['horse_name', 'evaluation', 'venue', 'race_name', 'horse_heads', 'horse_number', \
-              'popularity', 'jockey', 'jockey_winning_percentage', 'distance', 'horse_weight', \
-              'horse_weight_diff', 'winning_percentage']
+category_labels = ['horse_name', 'venue', 'race_name', 'jockey']
+numeric_labels = ['evaluation', 'horse_heads', 'horse_number', \
+	'popularity', 'jockey_winning_percentage', 'distance', 'horse_weight', \
+	'horse_weight_diff', 'winning_percentage']
 
-train_df = pd.read_csv('TrainData.csv', low_memory = False)
-train_order_data = train_df['order']
-train_feature_data  = train_df.drop('order', axis = 1)
+standard_scaler = preprocessing.StandardScaler()
 label_encoder = preprocessing.LabelEncoder()
-for label in labels:
-	train_feature_data[label] = label_encoder.fit_transform(train_feature_data[label])
+one_hot_encoder = preprocessing.OneHotEncoder()
 
+train_data_frame = pd.read_csv('TrainData.csv', low_memory = False)
+train_order_data_frame = train_data_frame['order']
+train_feature_data_frame  = train_data_frame.drop('order', axis = 1)
+category_data_frame = train_feature_data_frame[category_labels]
+category_data_frame = category_data_frame.apply(label_encoder.fit_transform)
+numeric_data_frame = train_feature_data_frame[numeric_labels]
+scaler = standard_scaler.fit(numeric_data_frame)
+numeric_data_frame = pd.DataFrame(scaler.transform(numeric_data_frame), index = numeric_data_frame.index.values, columns = numeric_data_frame.columns.values)
+train_feature_data_frame = category_data_frame.join(numeric_data_frame, how='outer')
 
-test_feature_data = pd.read_csv('TestData.csv', low_memory = False)
-names = list(test_feature_data['horse_name'].values.flatten())
-label_encoder = preprocessing.LabelEncoder()
-for label in labels:
-	test_feature_data[label] = label_encoder.fit_transform(test_feature_data[label])
+test_feature_data_frame = pd.read_csv('TestData.csv', low_memory = False)
+names = list(test_feature_data_frame['horse_name'].values.flatten())
+category_data_frame = test_feature_data_frame[category_labels]
+category_data_frame = category_data_frame.apply(label_encoder.fit_transform)
+numeric_data_frame = test_feature_data_frame[numeric_labels]
+scaler = standard_scaler.fit(numeric_data_frame)
+numeric_data_frame = pd.DataFrame(scaler.transform(numeric_data_frame), index = numeric_data_frame.index.values, columns = numeric_data_frame.columns.values)
+test_feature_data_frame = category_data_frame.join(numeric_data_frame, how='outer')
 
-print 'Training Data: ' + str(train_feature_data.shape[0])
-print 'Test Data: ' + str(test_feature_data.shape[0]) + '\n'
+print 'Training Data: ' + str(train_feature_data_frame.shape[0])
+print 'Test Data: ' + str(test_feature_data_frame.shape[0]) + '\n'
 
 random_forest = RandomForestRegressor(n_estimators = 210, max_depth = 50, n_jobs = 2)
 
-# feature_importances = random_forest.feature_importances_
-# if len(labels) == len(feature_importances):
-# 	for index in range(len(labels)):
-# 		print str(labels[index]) + ' : ' + str(round(feature_importances[index] * 100, 2)) + '[%]'
-#
-# print '\n'
-
-prediction_times = 10
+prediction_times = 1
 order_score_dictionary = {}
 
 for index in range(prediction_times):
-	random_forest.fit(train_feature_data, train_order_data)
-	predicts = random_forest.predict(test_feature_data)
+	random_forest.fit(train_feature_data_frame, train_order_data_frame)
+	predicts = random_forest.predict(test_feature_data_frame)
 	if len(predicts) == len(names):
 		predict_dictionary = {}
 		for name_index in range(len(names)):
